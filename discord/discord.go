95f864e91f2fdc7a02ea8476b0929649e7a237d1
justinnamilee <justin.nami.lee@gmail.com>
2025-06-24 16:13:34 -0300

    Quick hack to get in new env for template file, update indentation

--- discord/discord.go
+++ discord/discord.go
@@ -1,100 +1,101 @@
 package discord
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/mail"
-	"text/template"
+  "bytes"
+  "encoding/json"
+  "io"
+  "io/ioutil"
+  "net/http"
+  "net/mail"
+  "text/template"
 )
 
 type Session struct {
-	webhook string
+  webhook, template string
 }
 
 type TemplateParams struct {
-	Date, From, To, Subject, Body string
+  Date, From, To, Subject, Body string
 }
 
-func NewSession(discordWebhookUri string) (*Session, error) {
-	return &Session{
-		webhook: discordWebhookUri,
-	}, nil
+func NewSession(discordWebhookUri string, discordTemplate string) (*Session, error) {
+  return &Session{
+    webhook: discordWebhookUri,
+    template: discordTemplate,
+  }, nil
 }
 
 func (s *Session) Send(r io.Reader) error {
 
-	msg, err := s.ParseTemplate(r)
-	if err != nil {
-		return err
-	}
+  msg, err := s.ParseTemplate(r)
+  if err != nil {
+    return err
+  }
 
-	err = s.SendToDiscord(msg)
-	if err != nil {
-		return err
-	}
+  err = s.SendToDiscord(msg)
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
 func (s *Session) ParseTemplate(r io.Reader) (string, error) {
-	m, err := mail.ReadMessage(r)
-	if err != nil {
-		return "", err
-	}
+  m, err := mail.ReadMessage(r)
+  if err != nil {
+    return "", err
+  }
 
-	body, err := ioutil.ReadAll(m.Body)
-	if err != nil {
-		return "", err
-	}
+  body, err := ioutil.ReadAll(m.Body)
+  if err != nil {
+    return "", err
+  }
 
-	header := m.Header
-	ttParams := TemplateParams{
-		header.Get("Date"),
-		header.Get("From"),
-		header.Get("To"),
-		header.Get("Subject"),
-		string(body),
-	}
+  header := m.Header
+  ttParams := TemplateParams{
+    header.Get("Date"),
+    header.Get("From"),
+    header.Get("To"),
+    header.Get("Subject"),
+    string(body),
+  }
 
-	// Template
-	tdat, err := ioutil.ReadFile("/message.tt")
-	if err != nil {
-		return "", err
-	}
+  // Template
+  tdat, err := ioutil.ReadFile(s.discordTemplate)
+  if err != nil {
+    return "", err
+  }
 
-	t := template.Must(template.New("message").Parse(string(tdat)))
+  t := template.Must(template.New("message").Parse(string(tdat)))
 
-	buf := new(bytes.Buffer)
-	err = t.Execute(buf, ttParams)
-	if err != nil {
-		return "", err
-	}
+  buf := new(bytes.Buffer)
+  err = t.Execute(buf, ttParams)
+  if err != nil {
+    return "", err
+  }
 
-	return buf.String(), nil
+  return buf.String(), nil
 }
 
 func (s *Session) SendToDiscord(content string) error {
-	reqBody, err := json.Marshal(
-		map[string]string{
-			"content": content,
-		},
-	)
-	if err != nil {
-		return err
-	}
+  reqBody, err := json.Marshal(
+    map[string]string{
+      "content": content,
+    },
+  )
+  if err != nil {
+    return err
+  }
 
-	resp, err := http.Post(
-		s.webhook,
-		"application/json",
-		bytes.NewBuffer(reqBody),
-	)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+  resp, err := http.Post(
+    s.webhook,
+    "application/json",
+    bytes.NewBuffer(reqBody),
+  )
+  if err != nil {
+    return err
+  }
+  defer resp.Body.Close()
 
-	return nil
+  return nil
 }
--- main.go
+++ main.go
@@ -1,52 +1,56 @@
 package main
 
 import (
-	"log"
-	"os"
-	"time"
+  "log"
+  "os"
+  "time"
 
-	gosmtp "github.com/emersion/go-smtp"
-	"github.com/nullcosmos/discord-smtp-server/smtp"
-	"github.com/nullcosmos/discord-smtp-server/discord"
+  gosmtp "github.com/emersion/go-smtp"
+  "github.com/justinnamilee/discord-smtp-relay/smtp"
+  "github.com/justinnamilee/discord-smtp-relay/discord"
 )
 
 func main() {
-	discord, err := discord.NewSession(os.Getenv("DISCORD_WEBHOOK_URI"))
-	if err != nil {
-		log.Fatal(err)
-	}
+  discord, err := discord.NewSession(os.Getenv("WEBHOOK"), os.Getenv("TEMPLATE"))
 
-	backend, err := smtp.NewBackend(
-		discord,
-		os.Getenv("SMTP_USERNAME"),
-		os.Getenv("SMTP_PASSWORD"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	server := gosmtp.NewServer(backend)
+  backend, err := smtp.NewBackend(
+    discord,
+    os.Getenv("USERNAME"),
+    os.Getenv("PASSWORD"),
+  )
 
-	port := ":1025"
-	if os.Getenv("PORT") != "" {
-		port = ":" + os.Getenv("PORT")
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	host := "localhost"
-	if os.Getenv("HOST") != "" {
-		host = os.Getenv("HOST")
-	}
+  server := gosmtp.NewServer(backend)
 
-	server.Addr = port
-	server.Domain = host
-	server.ReadTimeout = 10 * time.Second
-	server.WriteTimeout = 10 * time.Second
-	server.MaxMessageBytes = 1024 * 1024
-	server.MaxRecipients = 50
-	server.AllowInsecureAuth = true
+  port := ":1025"
+  
+  if os.Getenv("PORT") != "" {
+    port = ":" + os.Getenv("PORT")
+  }
 
-	log.Println("Starting server at", server.Addr)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+  host := "localhost"
+  if os.Getenv("HOST") != "" {
+    host = os.Getenv("HOST")
+  }
+
+  server.Addr = port
+  server.Domain = host
+  server.ReadTimeout = 10 * time.Second
+  server.WriteTimeout = 10 * time.Second
+  server.MaxMessageBytes = 1024 * 1024
+  server.MaxRecipients = 50
+  server.AllowInsecureAuth = true
+
+  log.Println("Starting server at", server.Addr)
+
+  if err := server.ListenAndServe(); err != nil {
+    log.Fatal(err)
+  }
 }
